0fe2d83a13fa8e6049e54de7a579889e1da7f4cd
Erik Sundell <erik.sundell87@gmail.com>
2021-03-11 09:12:20 +0100

    change order of hooks

--- src/ConnectionConfig.tsx
+++ src/ConnectionConfig.tsx
@@ -18,7 +18,8 @@ import { AwsAuthType } from 'types';
 
 const toOption = (value: string) => ({ value, label: value });
 
-export interface ConnectionConfigProps<J = AwsAuthDataSourceJsonData, S = AwsAuthDataSourceSecureJsonData> extends DataSourcePluginOptionsEditorProps<J,S> {
+export interface ConnectionConfigProps<J = AwsAuthDataSourceJsonData, S = AwsAuthDataSourceSecureJsonData>
+  extends DataSourcePluginOptionsEditorProps<J, S> {
   standardRegions?: string[];
   loadRegions?: () => Promise<string[]>;
   defaultEndpoint?: string;
@@ -28,26 +29,12 @@ export interface ConnectionConfigProps<J = AwsAuthDataSourceJsonData, S = AwsAut
 export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionConfigProps) => {
   const [regions, setRegions] = useState((props.standardRegions || standardRegions).map(toOption));
 
-  useEffect(() => {
-    if (!props.loadRegions) {
-      return;
-    }
-
-    props.loadRegions().then(regions => setRegions(regions.map(toOption)));
-  }, [props.loadRegions]);
-
-  const options = props.options;
-  let profile = options.jsonData.profile;
-  if (profile === undefined) {
-    profile = options.database;
-  }
-  
-  const currentProvider = awsAuthProviderOptions.find(p => p.value === options.jsonData.authType);
+  const currentProvider = awsAuthProviderOptions.find((p) => p.value === options.jsonData.authType);
 
   // Component did mount
   useEffect(() => {
     // Make sure a authType exists in the current model
-    if(!currentProvider && awsAuthProviderOptions.length) {
+    if (!currentProvider && awsAuthProviderOptions.length) {
       props.onOptionsChange({
         ...options,
         jsonData: {
@@ -58,9 +45,26 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
     }
   }, []);
 
+  useEffect(() => {
+    if (!props.loadRegions) {
+      return;
+    }
+
+    props.loadRegions().then((regions) => setRegions(regions.map(toOption)));
+  }, [props.loadRegions]);
+
+  const options = props.options;
+  let profile = options.jsonData.profile;
+  if (profile === undefined) {
+    profile = options.database;
+  }
+
   // awsAllowedAuthProviders is supported in 7.5+
-  const awsAllowedAuthProviders: Array<AwsAuthType> = ((config as any).awsAllowedAuthProviders) ??
-    [AwsAuthType.Default,AwsAuthType.Keys,AwsAuthType.Credentials];
+  const awsAllowedAuthProviders: Array<AwsAuthType> = (config as any).awsAllowedAuthProviders ?? [
+    AwsAuthType.Default,
+    AwsAuthType.Keys,
+    AwsAuthType.Credentials,
+  ];
 
   return (
     <FieldSet label="Connection Details">
@@ -71,10 +75,10 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
       >
         <Select
           className="width-30"
-          value={ currentProvider || awsAuthProviderOptions[0]}
-          options={awsAuthProviderOptions.filter(opt => awsAllowedAuthProviders.includes(opt.value!))}
+          value={currentProvider || awsAuthProviderOptions[0]}
+          options={awsAuthProviderOptions.filter((opt) => awsAllowedAuthProviders.includes(opt.value!))}
           defaultValue={options.jsonData.authType}
-          onChange={option => {
+          onChange={(option) => {
             onUpdateDatasourceJsonDataOptionSelect(props, 'authType')(option);
           }}
         />
@@ -171,7 +175,7 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
       >
         <Select
           className="width-30"
-          value={regions.find(region => region.value === options.jsonData.defaultRegion)}
+          value={regions.find((region) => region.value === options.jsonData.defaultRegion)}
           options={regions}
           defaultValue={options.jsonData.defaultRegion}
           allowCustomValue={true}
