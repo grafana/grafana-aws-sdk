040e1fa06d5a3cfe0e308c3a87c99776219963db
Sarah Zinger <sarah.zinger@grafana.com>
2021-12-08 16:49:28 -0500

    Add Library Credential component

--- src/ConnectionConfig.tsx
+++ src/ConnectionConfig.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 import { Input, Select, InlineField, ButtonGroup, ToolbarButton, FieldSet } from '@grafana/ui';
 import {
   DataSourcePluginOptionsEditorProps,
@@ -13,6 +13,15 @@ import { AwsAuthDataSourceJsonData, AwsAuthDataSourceSecureJsonData, AwsAuthType
 import { awsAuthProviderOptions } from './providers';
 
 const toOption = (value: string) => ({ value, label: value });
+
+// TODO: move to grafana/ui
+const LibraryCredential = ({ credentialName, children }: { credentialName: string; children: ReactNode }) => {
+  return (
+    <>
+      <span data-lib-credential={credentialName}>{children}</span>
+    </>
+  );
+};
 
 export interface ConnectionConfigProps<J = AwsAuthDataSourceJsonData, S = AwsAuthDataSourceSecureJsonData>
   extends DataSourcePluginOptionsEditorProps<J, S> {
@@ -71,15 +80,17 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
         labelWidth={28}
         tooltip="Specify which AWS credentials chain to use."
       >
-        <Select
-          className="width-30"
-          value={currentProvider}
-          options={awsAuthProviderOptions.filter((opt) => awsAllowedAuthProviders.includes(opt.value!))}
-          defaultValue={options.jsonData.authType}
-          onChange={(option) => {
-            onUpdateDatasourceJsonDataOptionSelect(props, 'authType')(option);
-          }}
-        />
+        <LibraryCredential credentialName="authType">
+          <Select
+            className="width-30"
+            value={currentProvider}
+            options={awsAuthProviderOptions.filter((opt) => awsAllowedAuthProviders.includes(opt.value!))}
+            defaultValue={options.jsonData.authType}
+            onChange={(option) => {
+              onUpdateDatasourceJsonDataOptionSelect(props, 'authType')(option);
+            }}
+          />
+        </LibraryCredential>
       </InlineField>
       {options.jsonData.authType === 'credentials' && (
         <InlineField
@@ -87,55 +98,61 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
           labelWidth={28}
           tooltip="Credentials profile name, as specified in ~/.aws/credentials, leave blank for default."
         >
-          <Input
-            className="width-30"
-            placeholder="default"
-            value={profile}
-            onChange={onUpdateDatasourceJsonDataOption(props, 'profile')}
-          />
+          <LibraryCredential credentialName="profile">
+            <Input
+              className="width-30"
+              placeholder="default"
+              value={profile}
+              onChange={onUpdateDatasourceJsonDataOption(props, 'profile')}
+            />
+          </LibraryCredential>
         </InlineField>
       )}
 
       {options.jsonData.authType === 'keys' && (
         <>
           <InlineField label="Access Key ID" labelWidth={28}>
-            {props.options.secureJsonFields?.accessKey ? (
-              <ButtonGroup className="width-30">
-                <Input disabled placeholder="Configured" />
-                <ToolbarButton
-                  icon="edit"
-                  tooltip="Edit Access Key ID"
-                  type="button"
-                  onClick={onUpdateDatasourceResetOption(props as any, 'accessKey')}
+            <LibraryCredential credentialName="accessKey">
+              {props.options.secureJsonFields?.accessKey ? (
+                <ButtonGroup className="width-30">
+                  <Input disabled placeholder="Configured" />
+                  <ToolbarButton
+                    icon="edit"
+                    tooltip="Edit Access Key ID"
+                    type="button"
+                    onClick={onUpdateDatasourceResetOption(props as any, 'accessKey')}
+                  />
+                </ButtonGroup>
+              ) : (
+                <Input
+                  className="width-30"
+                  value={options.secureJsonData?.accessKey ?? ''}
+                  onChange={onUpdateDatasourceSecureJsonDataOption(props, 'accessKey')}
                 />
-              </ButtonGroup>
-            ) : (
-              <Input
-                className="width-30"
-                value={options.secureJsonData?.accessKey ?? ''}
-                onChange={onUpdateDatasourceSecureJsonDataOption(props, 'accessKey')}
-              />
-            )}
+              )}
+            </LibraryCredential>
           </InlineField>
 
           <InlineField label="Secret Access Key" labelWidth={28}>
-            {props.options.secureJsonFields?.secretKey ? (
-              <ButtonGroup className="width-30">
-                <Input disabled placeholder="Configured" />
-                <ToolbarButton
-                  icon="edit"
-                  type="button"
-                  tooltip="Edit Secret Access Key"
-                  onClick={onUpdateDatasourceResetOption(props as any, 'secretKey')}
+            <LibraryCredential credentialName="secretKey">
+              {props.options.secureJsonFields?.secretKey ? (
+                <ButtonGroup className="width-30">
+                  <Input disabled placeholder="Configured" />
+                  <ToolbarButton
+                    icon="edit"
+                    type="button"
+                    tooltip="Edit Secret Access Key"
+                    onClick={onUpdateDatasourceResetOption(props as any, 'secretKey')}
+                  />
+                </ButtonGroup>
+              ) : (
+                <Input
+                  className="width-30"
+                  value={options.secureJsonData?.secretKey ?? ''}
+                  onChange={onUpdateDatasourceSecureJsonDataOption(props, 'secretKey')}
                 />
-              </ButtonGroup>
-            ) : (
-              <Input
-                className="width-30"
-                value={options.secureJsonData?.secretKey ?? ''}
-                onChange={onUpdateDatasourceSecureJsonDataOption(props, 'secretKey')}
-              />
-            )}
+              )}
+            </LibraryCredential>
           </InlineField>
         </>
       )}
@@ -147,35 +164,41 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
             labelWidth={28}
             tooltip="Optionally, specify the ARN of a role to assume. Specifying a role here will ensure that the selected authentication provider is used to assume the specified role rather than using the credentials directly. Leave blank if you don't need to assume a role at all"
           >
-            <Input
-              className="width-30"
-              placeholder="arn:aws:iam:*"
-              value={options.jsonData.assumeRoleArn || ''}
-              onChange={onUpdateDatasourceJsonDataOption(props, 'assumeRoleArn')}
-            />
+            <LibraryCredential credentialName="assumeRoleArn">
+              <Input
+                className="width-30"
+                placeholder="arn:aws:iam:*"
+                value={options.jsonData.assumeRoleArn || ''}
+                onChange={onUpdateDatasourceJsonDataOption(props, 'assumeRoleArn')}
+              />
+            </LibraryCredential>
           </InlineField>
           <InlineField
             label="External ID"
             labelWidth={28}
             tooltip="If you are assuming a role in another account, that has been created with an external ID, specify the external ID here."
           >
-            <Input
-              className="width-30"
-              placeholder="External ID"
-              value={options.jsonData.externalId || ''}
-              onChange={onUpdateDatasourceJsonDataOption(props, 'externalId')}
-            />
+            <LibraryCredential credentialName="externalId">
+              <Input
+                className="width-30"
+                placeholder="External ID"
+                value={options.jsonData.externalId || ''}
+                onChange={onUpdateDatasourceJsonDataOption(props, 'externalId')}
+              />
+            </LibraryCredential>
           </InlineField>
         </>
       )}
       {!skipEndpoint && (
         <InlineField label="Endpoint" labelWidth={28} tooltip="Optionally, specify a custom endpoint for the service">
-          <Input
-            className="width-30"
-            placeholder={props.defaultEndpoint ?? 'https://{service}.{region}.amazonaws.com'}
-            value={options.jsonData.endpoint || ''}
-            onChange={onUpdateDatasourceJsonDataOption(props, 'endpoint')}
-          />
+          <LibraryCredential credentialName="endpoint">
+            <Input
+              className="width-30"
+              placeholder={props.defaultEndpoint ?? 'https://{service}.{region}.amazonaws.com'}
+              value={options.jsonData.endpoint || ''}
+              onChange={onUpdateDatasourceJsonDataOption(props, 'endpoint')}
+            />
+          </LibraryCredential>
         </InlineField>
       )}
       <InlineField
@@ -183,15 +206,17 @@ export const ConnectionConfig: FC<ConnectionConfigProps> = (props: ConnectionCon
         labelWidth={28}
         tooltip="Specify the region, such as for US West (Oregon) use ` us-west-2 ` as the region."
       >
-        <Select
-          className="width-30"
-          value={regions.find((region) => region.value === options.jsonData.defaultRegion)}
-          options={regions}
-          defaultValue={options.jsonData.defaultRegion}
-          allowCustomValue={true}
-          onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'defaultRegion')}
-          formatCreateLabel={(r) => `Use region: ${r}`}
-        />
+        <LibraryCredential credentialName="defaultRegion">
+          <Select
+            className="width-30"
+            value={regions.find((region) => region.value === options.jsonData.defaultRegion)}
+            options={regions}
+            defaultValue={options.jsonData.defaultRegion}
+            allowCustomValue={true}
+            onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'defaultRegion')}
+            formatCreateLabel={(r) => `Use region: ${r}`}
+          />
+        </LibraryCredential>
       </InlineField>
       {props.children}
     </FieldSet>
