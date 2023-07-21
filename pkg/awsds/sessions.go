9b983a2e829985eb808db6fe859aa9727e8a9d59
Sarah Zinger <sarah.zinger@grafana.com>
2023-07-21 09:39:04 -0400

    Move log back to debug

--- pkg/awsds/sessions.go
+++ pkg/awsds/sessions.go
@@ -106,6 +106,7 @@ func ReadAuthSettingsFromEnvironmentVariables() *AuthSettings {
 
 // Session factory.
 // Stubbable by tests.
+//
 //nolint:gocritic
 var newSession = func(cfgs ...*aws.Config) (*session.Session, error) {
 	return session.NewSession(cfgs...)
@@ -113,11 +114,13 @@ var newSession = func(cfgs ...*aws.Config) (*session.Session, error) {
 
 // STS credentials factory.
 // Stubbable by tests.
+//
 //nolint:gocritic
 var newSTSCredentials = stscreds.NewCredentials
 
 // EC2Metadata service factory.
 // Stubbable by tests.
+//
 //nolint:gocritic
 var newEC2Metadata = ec2metadata.New
 
@@ -267,7 +270,7 @@ func (sc *SessionCache) GetSession(c SessionConfig) (*session.Session, error) {
 	expiration := time.Now().UTC().Add(duration)
 	if c.Settings.AssumeRoleARN != "" && sc.authSettings.AssumeRoleEnabled {
 		// We should assume a role in AWS
-		backend.Logger.Info("Trying to assume role in AWS", "arn", c.Settings.AssumeRoleARN)
+		backend.Logger.Debug("Trying to assume role in AWS", "arn", c.Settings.AssumeRoleARN)
 
 		cfgs := []*aws.Config{
 			{
